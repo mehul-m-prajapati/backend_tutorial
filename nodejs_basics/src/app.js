3ca2c882523198bbbc636cdbe5281ca8e1393fe4
Mehul <mehulprajapati2802@gmail.com>
2025-06-25 19:07:46 +0530

    added middlewares

--- nodejs_basics/src/app.js
+++ nodejs_basics/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
+
 import connectDB from './db/index.js';
 import cors from "cors";
 import cookieParser from "cookie-parser";
---
+++ nodejs_basics/src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+
+    try {
+
+        if (!localFilePath) return null
+
+        // upload the file on cloudinary
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+
+        console.log("file is uploaded on cloudinary ", result.url);
+
+        // Remove file from local after upload
+        fs.unlinkSync(localFilePath);
+
+        return result;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        console.log('cloudinary err', error);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
